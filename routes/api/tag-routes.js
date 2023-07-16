fa371a8d5a3d9177e6e24f975214ebf15463e888
Kevin Silveira <k.ironhide9@yahoo.com>
2023-07-16 18:12:54 -0400

    updated route files and updated node modules through package.json/package-lock.json files. Models have been updated with new foreign key. connection.js has been linked to new mysql database. .gitignore has been added to hide .env file with mysql database name/user/psswrd.

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,77 @@
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
+
+// The `/api/tags` endpoint
+
+router.get("/", async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: { model: Product },
+    });
+    if (!tagData) {
+      res.status(404).json("No tag found with that id!");
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    const updatedTagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    })
+    res.status(200).json({message: 'Category successfully UPDATED', updatedTagData});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json("Product successfully DELETED");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
